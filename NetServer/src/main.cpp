4ac746f3f277152c073d705edfc2cdc5caa8e2e8
MikolajKos <niko.kosiorek@gmail.com>
2026-05-22 14:07:40 +0200

    feat: entry point for server created in main.cpp

--- NetServer/src/main.cpp
+++ NetServer/src/main.cpp
@@ -1,3 +1,23 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include "MasterServer.hpp"
+
+using namespace std::literals::chrono_literals;
+
 int main() {
-  return 0;
+    std::cout << "[MASTER] Starting the server...\n";
+
+    MasterServer server(60000);
+    
+    server.Start();
+    std::cout << "[MASTER] The server is running and waiting for Workers\n";
+    
+    while(true) {
+        server.Update();
+        std::this_thread::sleep_for(1ms);
+    }
+
+    return 0;
 }
