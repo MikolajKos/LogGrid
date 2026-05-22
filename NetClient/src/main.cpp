09fb4f4e7cae7859d246724cdf53026e832509fb
MikolajKos <niko.kosiorek@gmail.com>
2026-05-22 15:37:01 +0200

    wip: entry-points implementation

--- NetClient/src/main.cpp
+++ NetClient/src/main.cpp
@@ -1,3 +1,35 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include "WorkerClient.hpp"
+
+#define LOOPBACK "127.0.0.1"
+
+using namespace std::literals::chrono_literals;
+
 int main() {
-  return 0;
+    WorkerClient worker;
+
+    worker.Connect(LOOPBACK, 60000);
+    std::cout << "[WORKER] Connecting to the server...\n";
+
+    // Wait for connection to establish
+    int connAttempts = 0;
+    while(!worker.IsConnected() && connAttempts < 10) {
+        std::this_thread::sleep_for(500ms);
+    }
+    
+    if (worker.IsConnected()) {
+        // Connection [ACK]
+        worker.SendMessage(LogSystem::LogSearchMsg::Worker_Hello);
+
+        std::cout << "[WORKER] Connection established\n";
+
+        while (worker.IsConnected()) {
+            std::this_thread::sleep_for(200ms);
+        }
+    }
+
+    return 0;
 }
